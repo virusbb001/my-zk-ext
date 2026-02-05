09f4078f0e81b75160d71dd99aeb617893666a56
virus <2017646+virusbb001@users.noreply.github.com>
2026-02-05 23:03:24 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "my zk external commands";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = [
+            pkgs.deno
+          ];
+        };
+      }
+    );
+}
