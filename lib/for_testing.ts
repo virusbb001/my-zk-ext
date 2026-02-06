c09e941277bcf6de38b1fd83af909023fa1fb39d
virus <2017646+virusbb001@users.noreply.github.com>
2026-02-07 03:12:44 +0900

    add some util for testing

---
+++ lib/for_testing.ts
@@ -0,0 +1,72 @@
+import { join } from "@std/path";
+import { emptyDir, copy, walk } from "@std/fs";
+import { TestTemplates } from "./test_consts.ts";
+
+export interface EntriesDiff {
+  added: string[],
+  removed: string[],
+  edited: string[],
+}
+
+export function isDiffEmpty (diff: EntriesDiff) {
+  return (diff.added.length + diff.removed.length + diff.edited.length) === 0;
+}
+
+/**
+ * Detect diff of directory.
+ */
+export async function diffDirectory (oldDir: string, newDir: string): Promise<EntriesDiff> {
+  const aEntries = await Array.fromAsync(walk(oldDir));
+  const bEntries = await Array.fromAsync(walk(newDir));
+  const oldEntriesMap = new Map(aEntries.map(entry => [entry.path, entry]));
+  const newEntriesMap = new Map(bEntries.map(entry => [entry.path, entry]));
+
+  const oldPath = new Set(aEntries.map(entry => entry.path.replace(oldDir, "")));
+  const newPath = new Set(bEntries.map(entry => entry.path.replace(newDir, "")))
+  const added = Array.from(newPath.difference(oldPath));
+  const removed = Array.from(oldPath.difference(newPath));
+  const samePath = Array.from(newPath.intersection(oldPath));
+  const editedFlag = await Promise.all(samePath.map(async path => {
+    const oldEntry = oldEntriesMap.get(join(oldDir, path))!;
+    const newEntry = newEntriesMap.get(join(newDir, path))!;
+    if (oldEntry.isDirectory && newEntry.isDirectory) {
+      return false;
+    }
+    if (oldEntry.isDirectory || newEntry.isDirectory) {
+      return true;
+    }
+
+    const oldContent = await Deno.readFile(join(oldDir, path));
+    const newContent = await Deno.readFile(join(newDir, path));
+    if (oldContent.length !== newContent.length) {
+      return true;
+    }
+    return oldContent.some((value, index) => value !== newContent[index]);
+  }));
+  const edited = samePath.filter((_, index) => editedFlag[index]);
+
+  return {
+    added,
+    removed,
+    edited
+  };
+};
+
+// simply, delete and copy
+export async function syncDirectory (src: string, dest: string) {
+  await emptyDir(dest);
+  await copy(src, dest, { overwrite: true });
+}
+
+if(import.meta.main) {
+  console.log(await Array.fromAsync(walk(join(TestTemplates, "notebooks"))));
+
+  // const tmpDir = await Deno.makeTempDir();
+  // try {
+  //   await syncDirectory(TestTemplates, tmpDir);
+  //   await Deno.remove(tmpDir, { recursive: true });
+  // } catch(e) {
+  //   console.log(e);
+  //   console.log(`Please remove ${tmpDir}`);
+  // }
+}
---
+++ lib/for_testing_test.ts
@@ -0,0 +1,178 @@
+import { syncDirectory, diffDirectory, isDiffEmpty } from "./for_testing.ts";
+import { TestTemplates } from "./test_consts.ts";
+import * as path from "@std/path";
+import { emptyDir } from "@std/fs";
+import { assertEquals, assert } from "@std/assert";
+
+const TmpDir = Deno.env.get("TMPDIR") ?? "/tmp";
+
+Deno.test({
+  name: "isDiffEmpty",
+  fn () {
+    assert(isDiffEmpty({
+      added: [],
+      removed: [],
+      edited: []
+    }), "empty")
+    assert(!isDiffEmpty({
+      added: ["/a/1.txt"],
+      removed: [],
+      edited: []
+    }), "added")
+    assert(!isDiffEmpty({
+      added: [],
+      removed: ["/a/1.txt"],
+      edited: []
+    }), "removed")
+    assert(!isDiffEmpty({
+      added: [],
+      removed: [],
+      edited: ["/a/1.txt"]
+    }), "edited")
+  }
+});
+
+Deno.test({
+  name: "diffDirectory should return empty diff",
+  permissions: {
+    read: true,
+    write: [TmpDir]
+  },
+  async fn () {
+    await emptyDir(TmpDir);
+    const oldDir = path.join(TmpDir, "old");
+    await Deno.mkdir(oldDir);
+    const newDir = path.join(TmpDir, "new");
+    await Deno.mkdir(newDir);
+
+    await Deno.mkdir(path.join(oldDir, "a"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(oldDir, "a", "b"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "b", "2.txt"), "a/b-2");
+
+    await Deno.mkdir(path.join(newDir, "a"));
+    await Deno.writeTextFile(path.join(newDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(newDir, "a", "b"));
+    await Deno.writeTextFile(path.join(newDir, "a", "b", "2.txt"), "a/b-2");
+
+    const result = await diffDirectory(oldDir, newDir);
+    assertEquals(result.added.length, 0);
+    assertEquals(result.removed.length, 0);
+    assertEquals(result.edited.length, 0);
+  }
+});
+
+Deno.test({
+  name: "diffDirectory should return removed files",
+  permissions: {
+    read: true,
+    write: [TmpDir]
+  },
+  async fn () {
+    await emptyDir(TmpDir);
+    const oldDir = path.join(TmpDir, "old");
+    await Deno.mkdir(oldDir);
+    const newDir = path.join(TmpDir, "new");
+    await Deno.mkdir(newDir);
+
+    await Deno.mkdir(path.join(oldDir, "a"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(oldDir, "a", "b"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "b", "2.txt"), "a/b-2");
+
+    await Deno.mkdir(path.join(newDir, "a"));
+    await Deno.writeTextFile(path.join(newDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(newDir, "a", "b"));
+
+    const result = await diffDirectory(oldDir, newDir);
+    assertEquals(result.added.length, 0);
+    assertEquals(result.removed, ["/a/b/2.txt"]);
+    assertEquals(result.edited.length, 0);
+  }
+});
+
+Deno.test({
+  name: "diffDirectory should return added files",
+  permissions: {
+    read: true,
+    write: [TmpDir]
+  },
+  async fn () {
+    await emptyDir(TmpDir);
+    const oldDir = path.join(TmpDir, "old");
+    await Deno.mkdir(oldDir);
+    const newDir = path.join(TmpDir, "new");
+    await Deno.mkdir(newDir);
+
+    await Deno.mkdir(path.join(oldDir, "a"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(oldDir, "a", "b"));
+
+    await Deno.mkdir(path.join(newDir, "a"));
+    await Deno.writeTextFile(path.join(newDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(newDir, "a", "b"));
+    await Deno.writeTextFile(path.join(newDir, "a", "b", "2.txt"), "a/b-2");
+
+    const result = await diffDirectory(oldDir, newDir);
+    assertEquals(result.added, ["/a/b/2.txt"]);
+    assertEquals(result.removed.length, 0);
+    assertEquals(result.edited.length, 0);
+  }
+});
+
+Deno.test({
+  name: "diffDirectory should return edited files",
+  permissions: {
+    read: true,
+    write: [TmpDir]
+  },
+  async fn () {
+    const tmpDir = await Deno.makeTempDir();
+    await emptyDir(tmpDir);
+    const oldDir = path.join(tmpDir, "old");
+    await Deno.mkdir(oldDir);
+    const newDir = path.join(tmpDir, "new");
+    await Deno.mkdir(newDir);
+
+    await Deno.mkdir(path.join(oldDir, "a"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "1.txt"), "a-1");
+    await Deno.mkdir(path.join(oldDir, "a", "b"));
+    await Deno.writeTextFile(path.join(oldDir, "a", "b", "2.txt"), "a/b-2");
+
+    await Deno.mkdir(path.join(newDir, "a"));
+    await Deno.writeTextFile(path.join(newDir, "a", "1.txt"), "a-1-edited");
+    await Deno.mkdir(path.join(newDir, "a", "b"));
+    await Deno.writeTextFile(path.join(newDir, "a", "b", "2.txt"), "a/b-2");
+
+    const result = await diffDirectory(oldDir, newDir);
+    assertEquals(result.added.length, 0);
+    assertEquals(result.removed.length, 0);
+    assertEquals(result.edited, ["/a/1.txt"]);
+
+    await Deno.remove(tmpDir, { recursive: true })
+  }
+});
+
+
+Deno.test({
+  name: "syncDirectory",
+  ignore: false,
+  permissions: {
+    read: true,
+    write: [TmpDir]
+  },
+  async fn () {
+    const tmpDir = await Deno.makeTempDir();
+    const notebooksTemplate = path.join(TestTemplates, "notebooks");
+    try {
+      await syncDirectory(notebooksTemplate, tmpDir + "/");
+      const diff = await diffDirectory(notebooksTemplate, tmpDir);
+      assert(isDiffEmpty(diff));
+
+      await Deno.remove(tmpDir, { recursive: true });
+    } catch (e) {
+      console.log(`Remove ${tmpDir}`);
+      throw e;
+    }
+  }
+});
