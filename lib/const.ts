068910d5c4cf046a2ae70a6835bf58af93a8270b
virus <2017646+virusbb001@users.noreply.github.com>
2026-02-07 14:26:58 +0900

    split installing template to other function

--- lib/const.ts
+++ lib/const.ts
@@ -1,1 +1,5 @@
+import * as path from "@std/path";
+
 export const NotebookEnv = "ZK_NOTEBOOK_DIR";
+export const CommandName = "my-zk-ext";
+export const TemplatesDir = path.join(import.meta.dirname!, "..", "templates");
--- tasks/init.ts
+++ tasks/init.ts
@@ -1,13 +1,23 @@
 import { exists } from "@std/fs";
 import { Command } from "@cliffy/command";
 import * as path from "@std/path";
+import { parse } from "@std/toml";
 import { type GlobalOptions, searchNotebooks } from "../lib/index.ts"
+import { CommandName, TemplatesDir } from "../lib/const.ts";
 export const command = new Command<GlobalOptions>()
 .description("initialize zk notebooks for use task")
 .action(async (opts) => {
   await init(opts.notebookDir);
 })
 ;
+
+async function addTemplate(notebooks: string) {
+  const src = path.join(TemplatesDir, "tasks.md");
+  const dest = path.join(notebooks, ".zk", "templates", "tasks.md");
+  if (!(await exists(dest))) {
+    await Deno.copyFile(src, dest);
+  }
+}
 
 export async function init(
   notebookDir?: string
@@ -16,10 +26,6 @@ export async function init(
   if (!notebooks) {
     throw new Error("Failed to find zk directory");
   }
-  const src = path.join(import.meta.dirname!, "..", "templates", "tasks.md");
-  const dest = path.join(notebooks, ".zk", "templates", "tasks.md");
-  if (!(await exists(dest))) {
-    await Deno.copyFile(src, dest);
-  }
+  addTemplate(notebooks);
   // TODO: set up `zk task` alias
 };
