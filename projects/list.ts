3409167030a451da5736fcc635864fe1448055d6
virus <2017646+virusbb001@users.noreply.github.com>
2026-02-12 17:06:47 +0900

    avoid search in getProjects

--- projects/list.ts
+++ projects/list.ts
@@ -16,7 +16,11 @@ Example: ${CommandName} project list --zk -- --format=json
     .option("--zk", "Use `zk list`")
     .action(async function (opts) {
       const zkOpts = this.getLiteralArgs();
-      const files = await getProjects(opts.notebookDir);
+      const notebookDir = await searchNotebooks(opts.notebookDir);
+      if (!notebookDir) {
+        throw new Error("Notebooks not found");
+      }
+      const files = await getProjects(notebookDir);
       const projects = files.map((file) => path.basename(file, ".md"));
       if (opts.zk) {
         // TODO: emulate 0 files
@@ -30,12 +34,8 @@ Example: ${CommandName} project list --zk -- --format=json
 /**
  * return absolute paths of project files.
  */
-export async function getProjects(notebooks?: string): Promise<string[]> {
-  const notebookDir = await searchNotebooks(notebooks);
-  if (!notebookDir) {
-    throw new Error("Notebooks not found");
-  }
-  const projectDir = path.join(notebookDir, ProjectsDir);
+export async function getProjects(notebooks: string): Promise<string[]> {
+  const projectDir = path.join(notebooks, ProjectsDir);
   const entries = await Array.fromAsync(Deno.readDir(projectDir));
   const files = entries.filter((entry) => entry.isFile).map((entry) =>
     path.join(projectDir, entry.name)
