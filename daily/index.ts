f699e34e48dff4b824c0a5f24342280850f17437
virus <2017646+virusbb001@users.noreply.github.com>
2026-03-12 12:53:08 +0900

    add split command

--- daily/index.ts
+++ daily/index.ts
@@ -2,6 +2,7 @@ import { Command } from "@cliffy/command";
 import { GlobalOptions } from "../lib/index.ts";
 import { newCommand } from "./new.ts";
 import { list } from "./list.ts";
+import { split } from "./split.ts";
 
 export function daily() {
   return new Command<GlobalOptions>()
@@ -10,5 +11,6 @@ export function daily() {
       this.showHelp();
     })
     .command("new", newCommand())
-    .command("list", list());
+    .command("list", list())
+    .command("split", split());
 }
